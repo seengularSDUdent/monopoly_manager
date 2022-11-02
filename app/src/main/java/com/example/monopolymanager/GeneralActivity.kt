68b7a3d7aca66c0cdf5c6cf829ffab8796e494c6
jasymyq <200107026@stu.sdu.edu.kz>
2022-11-02 16:03:55 +0600

    0.0.4 Nurbek

---
+++ app/src/main/java/com/example/monopolymanager/GeneralActivity.kt
@@ -0,0 +1,11 @@
+package com.example.monopolymanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GeneralActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_general)
+    }
+}
