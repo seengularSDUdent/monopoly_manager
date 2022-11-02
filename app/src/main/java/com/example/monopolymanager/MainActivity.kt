9a3fbb14ed253854878acb7eb71e8833bc94b201
jasymyq <200107026@stu.sdu.edu.kz>
2022-11-02 13:31:31 +0600

    0.0.2 Nurbek

--- app/src/main/java/com/example/monopolymanager/MainActivity.kt
+++ app/src/main/java/com/example/monopolymanager/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.monopolymanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        getSupportActionBar()?.hide()
         setContentView(R.layout.activity_main)
     }
 }
